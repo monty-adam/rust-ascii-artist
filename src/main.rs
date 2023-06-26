f214ff55d14932b8a3037f00eca546a154e08117
Adam Cowan <adamcowan@adams-mbp.home>
2023-06-26 21:03:52 +0100

    renders before printing image

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 //! An example of opening an image.
 extern crate image;
+extern crate termion;
 
-use std::{env, path::Path};
-use image::{GenericImageView, Pixel};
+use image::{imageops::FilterType, GenericImageView, Pixel};
+use std::{
+    env,
+    io::{stdout, Write},
+    path::Path,
+};
+use termion::{clear, color as tty_color, raw::IntoRawMode};
 
 fn main() {
     let file = if env::args().count() == 2 {
@@ -15,18 +21,38 @@ fn main() {
     // ```open``` returns a dynamic image.
     let im = image::open(&Path::new(&file)).unwrap();
 
+    // We go to raw mode to make the control over the terminal more fine-grained.
+    let mut stdout = stdout().into_raw_mode().unwrap();
+
+    let termsize = termion::terminal_size().ok();
+    let (termwidth, _termheight) = termsize.unwrap();
+
+    let im = im.resize_exact(
+        u32::try_from(termwidth)
+            .map(|w| ((im.height() / im.width()) * w))
+            .unwrap(),
+        u32::try_from(termwidth / 2).unwrap(),
+        FilterType::Triangle,
+    );
+
+    let mut ascii_art = String::new();
     for (x, y, color) in im.pixels() {
-        println!("x: {x}, y: {y}");
-
         let color = color.to_rgb();
-        match color.0 {
-            [r, g, b] => {
-                println!("r: {r}, g: {g}, b: {b}");
+        let ascii = match color.0 {
+            [red, green, blue] => {
+                format!(
+                    "{}{}\u{2588} ",
+                    termion::cursor::Goto(
+                        u16::try_from(x + 1).unwrap(),
+                        u16::try_from(y + 1).unwrap()
+                    ),
+                    tty_color::Fg(tty_color::Rgb(red, green, blue))
+                )
             }
-        }
-
-        if x > 2 {
-            break;
-        }
+        };
+        ascii_art.push_str(&ascii);
     }
+
+    write!(&mut stdout, "{}", clear::All).unwrap();
+    stdout.write(ascii_art.as_bytes()).unwrap();
 }
