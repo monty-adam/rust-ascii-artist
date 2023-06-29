cd675594aefec490cb989c36d9c79e5baebf0411
Adam Cowan <adamcowan@adams-mbp.home>
2023-06-30 00:03:17 +0100

    video killed the radio star

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ use std::{
 };
 use termion::{clear, color as tty_color, raw::IntoRawMode};
 
+const NUM_OF_FRAMES: u32 = 4;
+
 fn main() {
     let file = if env::args().count() == 2 {
         env::args().nth(1).unwrap()
@@ -19,7 +21,10 @@ fn main() {
 
     // Use the open function to load an image from a Path.
     // ```open``` returns a dynamic image.
-    let im = image::open(&Path::new(&file)).unwrap();
+    let im = match image::open(&Path::new(&file)) {
+        Ok(buff) => buff.into_rgb8(),
+        Err(err) => panic!("Failed to read image file: {:?}", err),
+    };
 
     // We go to raw mode to make the control over the terminal more fine-grained.
     let mut stdout = stdout().into_raw_mode().unwrap();
@@ -27,29 +32,34 @@ fn main() {
     let termsize = termion::terminal_size().ok();
     let (termwidth, _termheight) = termsize.unwrap();
 
-    let im = if im.width() > im.height() {
-        im.rotate90()
-    } else {
-        im
-    };
-    let im = im.resize_exact(
-        u32::try_from(termwidth)
-            .map(|w| ((im.height() / im.width()) * w))
-            .unwrap(),
-        u32::try_from(termwidth / 2).unwrap(),
-        FilterType::Triangle,
-    );
+    // let im = if im.width() > im.height() {
+    //     im.rotate90()
+    // } else {
+    //     im
+    // };
+    // let im = im.resize_exact(
+    //     u32::try_from(termwidth)
+    //         .map(|w| ((im.height() / im.width()) * w))
+    //         .unwrap(),
+    //     u32::try_from(termwidth / 2).unwrap(),
+    //     FilterType::Triangle,
+    // );
 
-    let mut ascii_art = String::new();
+    let mut scenes: Vec<String> = Vec::with_capacity(usize::try_from(NUM_OF_FRAMES).unwrap());
+    let total_pixels = im.height() * im.width();
     for (x, y, color) in im.pixels() {
-        let color = color.to_rgb();
+        let x = x + 1
+        let y = y + 1
+        let mut ascii_art = if total_pixels % x * y == 0 {
+            return String::new()
+        }
         let ascii = match color.0 {
             [red, green, blue] => {
                 format!(
                     "{}{}\u{2588} ",
                     termion::cursor::Goto(
-                        u16::try_from(x + 1).unwrap(),
-                        u16::try_from(y + 1).unwrap()
+                        u16::try_from(x).unwrap(),
+                        u16::try_from(y).unwrap()
                     ),
                     tty_color::Fg(tty_color::Rgb(red, green, blue))
                 )
@@ -58,6 +68,8 @@ fn main() {
         ascii_art.push_str(&ascii);
     }
 
-    write!(&mut stdout, "{}", clear::All).unwrap();
-    stdout.write(ascii_art.as_bytes()).unwrap();
+    for scene in & scenes {
+        write!(&mut stdout, "{}", clear::All).unwrap();
+        stdout.write(scene.as_bytes()).unwrap();
+    }
 }
