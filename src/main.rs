4ac560d7266d27454bae52e6f6ba10fcc5c24967
Adam Cowan <adamcowan@adams-mbp.home>
2023-07-02 00:01:23 +0100

    animated works

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@
 extern crate image;
 extern crate termion;
 
-use image::{imageops::FilterType, GenericImageView, Pixel};
 use std::{
     env,
     io::{stdout, Write},
     path::Path,
+    {thread, time},
 };
 use termion::{clear, color as tty_color, raw::IntoRawMode};
 
@@ -22,54 +22,51 @@ fn main() {
     // Use the open function to load an image from a Path.
     // ```open``` returns a dynamic image.
     let im = match image::open(&Path::new(&file)) {
-        Ok(buff) => buff.into_rgb8(),
+        Ok(buff) => buff,
         Err(err) => panic!("Failed to read image file: {:?}", err),
     };
+    let im = im.into_rgb8();
+
+    let termsize = termion::terminal_size().ok();
+    let (_termwidth, _termheight) = termsize.unwrap();
+
+    let total_pixels = im.width() * im.height();
+    println!("Total Pixels: {total_pixels}");
+
+    let mut frames: Vec<String> = Vec::with_capacity(NUM_OF_FRAMES as usize);
+    let mut pixels = im.enumerate_pixels();
+    for _frame in 0..NUM_OF_FRAMES {
+        let mut ascii_art = String::new();
+
+        for y in 0..(im.height() / NUM_OF_FRAMES) {
+            for x in 0..(im.width()) {
+                let (_x, _y, color) = pixels.next().unwrap();
+                let x = 1 + x;
+                let y = 1 + y;
+                let ascii = match color.0 {
+                    [red, green, blue] => {
+                        format!(
+                            "{}{}\u{2588} ",
+                            termion::cursor::Goto(x as u16, y as u16),
+                            tty_color::Fg(tty_color::Rgb(red, green, blue))
+                        )
+                    }
+                };
+
+                ascii_art.push_str(&ascii);
+            }
+        }
+
+        frames.push(ascii_art);
+    }
 
     // We go to raw mode to make the control over the terminal more fine-grained.
     let mut stdout = stdout().into_raw_mode().unwrap();
 
-    let termsize = termion::terminal_size().ok();
-    let (termwidth, _termheight) = termsize.unwrap();
-
-    // let im = if im.width() > im.height() {
-    //     im.rotate90()
-    // } else {
-    //     im
-    // };
-    // let im = im.resize_exact(
-    //     u32::try_from(termwidth)
-    //         .map(|w| ((im.height() / im.width()) * w))
-    //         .unwrap(),
-    //     u32::try_from(termwidth / 2).unwrap(),
-    //     FilterType::Triangle,
-    // );
-
-    let mut scenes: Vec<String> = Vec::with_capacity(usize::try_from(NUM_OF_FRAMES).unwrap());
-    let total_pixels = im.height() * im.width();
-    for (x, y, color) in im.pixels() {
-        let x = x + 1
-        let y = y + 1
-        let mut ascii_art = if total_pixels % x * y == 0 {
-            return String::new()
-        }
-        let ascii = match color.0 {
-            [red, green, blue] => {
-                format!(
-                    "{}{}\u{2588} ",
-                    termion::cursor::Goto(
-                        u16::try_from(x).unwrap(),
-                        u16::try_from(y).unwrap()
-                    ),
-                    tty_color::Fg(tty_color::Rgb(red, green, blue))
-                )
-            }
-        };
-        ascii_art.push_str(&ascii);
-    }
-
-    for scene in & scenes {
-        write!(&mut stdout, "{}", clear::All).unwrap();
-        stdout.write(scene.as_bytes()).unwrap();
+    write!(&mut stdout, "{}", clear::All).unwrap();
+    let ten_millis = time::Duration::from_millis(250);
+    while let Some(frame) = frames.pop() {
+        stdout.write(frame.as_bytes()).unwrap();
+        thread::sleep(ten_millis);
     }
 }
